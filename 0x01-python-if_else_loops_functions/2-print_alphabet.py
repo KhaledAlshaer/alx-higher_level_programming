080c048789f114476a874a83102afa43319f136e
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-10-31 11:10:28 -0700

    2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-output = ""
-for char_code in range(ord('a'), ord('z') + 1):
-    output += chr(char_code)
-print(output, end='')
+for i in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(i), end="")
