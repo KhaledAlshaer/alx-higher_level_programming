edd53fb5f2dd216b2de9981ddb29827d0fb49bfa
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-11-27 11:32:54 -0800

    4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
+# Define the Rectangle class
 class Rectangle:
     def __init__(self, width=0, height=0):
+        """Initialize the Rectangle object with optional width and height."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Getter method for width."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter method for width with type and value checks."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -20,10 +24,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Getter method for height."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter method for height with type and value checks."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
@@ -32,23 +38,27 @@ class Rectangle:
             self.__height = value
 
     def area(self):
+        """Calculate and return the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Calculate and return the perimeter of the rectangle."""
         return 2 * (self.__width + self.__height) if (
             self.__width > 0 and self.__height > 0
         ) else 0
 
     def __str__(self):
+        """Return a string representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
+        """Return a string representation that can recreate the object."""
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
-
 if __name__ == "__main__":
+    # Example usage and testing
     my_rectangle = Rectangle(2, 4)
     print(str(my_rectangle))
     print("--")
@@ -59,7 +69,7 @@ if __name__ == "__main__":
     print(hex(id(my_rectangle)))
     print("--")
 
-    # create new instance based on representation
+    # Create a new instance based on representation
     new_rectangle = eval(repr(my_rectangle))
     print(str(new_rectangle))
     print("--")
@@ -70,5 +80,6 @@ if __name__ == "__main__":
     print(hex(id(new_rectangle)))
     print("--")
 
+    # Check if the new instance is equal and of the same type
     print(new_rectangle is my_rectangle)
     print(type(new_rectangle) is type(my_rectangle))
