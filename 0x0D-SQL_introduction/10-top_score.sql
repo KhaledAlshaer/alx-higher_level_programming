fb79e3c11e9d7b223e695582a107dc9bb013937d
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-01-29 08:16:07 -0800

    10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+-- script that lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
