30180e389ff7c3c486f364e71bf91d22bc0951fa
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-02-07 07:38:19 -0800

    1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd'
-iGRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
 WITH GRANT OPTION;
